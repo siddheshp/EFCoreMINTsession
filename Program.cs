310df5cad5b5af7d38916361406db85afc6076de
Siddhesh <siddheshpg@gmail.com>
2020-04-08 19:27:24 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCoreConApp
+{
+    class Program
+    {
+        static IConfiguration BuildConfiguration(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appSettings.json", false, true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .Build();
+            return configuration;
+        }
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = BuildConfiguration(args);
+            Console.WriteLine(configuration["MOD"]);
+            Console.WriteLine(configuration.GetConnectionString("NWConnection"));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Entity Framwork with .NET Core 3.1
+
+- dotnet new console --name EFCoreConApp
+
+#### Packages for the project
+- dotnet add package Microsoft.Extensions.Configuration --version 3.1.3
+- dotnet add package Microsoft.Extensions.Configuration.Json --version 3.1.3
+- dotnet add package Microsoft.Extensions.Configuration.EnvironmentVariables --version 3.1.3
+- dotnet add package Microsoft.Extensions.Configuration.CommandLine --version 3.1.3
+- dotnet add package Microsoft.Extensions.Configuration.FileExtensions --version 3.1.3
+- dotnet add package Microsoft.Extensions.DependencyInjection --version 3.1.3
+
+#### Git branch
+
+- 1) Configuration
+- 2) Entities and DbContext
+- 3) Service Config
+- 4) Querying data
+- 5) DML Concurrency
+- 6) Second DI
